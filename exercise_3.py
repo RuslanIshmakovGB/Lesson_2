e29ca49e0160e491ec81789e810fb3191a72aaee
RuslanIshmakovGB <104893687+RuslanIshmakovGB@users.noreply.github.com>
2022-08-11 19:17:13 +0300

    Add files via upload

---
+++ exercise_3.py
@@ -0,0 +1,32 @@
+"""
+3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите два варианта решения: через list и через dict.
+
+Пример:
+Введите номер месяца: 10
+Результат через список: Осень
+Результат через словарь: Осень
+"""
+number = int(input("Enter month number: "))
+if 12 >= number >= 1:
+    month_dict = {1: 'January',
+                  2: 'February',
+                  3: 'March',
+                  4: 'April',
+                  5: 'May',
+                  6: 'June',
+                  7: 'July',
+                  8: 'August',
+                  9: 'September',
+                  10: 'October',
+                  11: 'November',
+                  12: 'December'}
+    month_list = list(month_dict.values())
+    for i, el in enumerate(month_list):
+        if i == number - 1:
+            print(f"Month from list is {month_list[i]}")
+            break
+    print(f"Month from dict is {month_dict[number]}")
+else:
+    print("You made a mistake")
